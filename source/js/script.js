12bf1a6fcd9371dcead33a1f60b661af00d4aa58
Ivan Castillo <ivan.castillo@triquimas.cr>
2022-10-31 09:11:00 -0600

    fix init

--- source/js/script.js
+++ source/js/script.js
@@ -6,8 +6,10 @@ AOS.init({
   once: true,
 });
 
-(function () {
+(function ($) {
   "use strict";
+
+  $(".videoplay").modalVideo();
 
   $(window).scroll(function () {
     var $this = $(this),
@@ -86,12 +88,79 @@ AOS.init({
     $(this).parent(".btn").addClass("active");
   });
 
+  $(".portfolio-single-slider").slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  $(".clients-logo").slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  $(".testimonial-wrap").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+
+  $(".portfolio-gallery").each(function () {
+    $(this)
+      .find(".popup-gallery")
+      .magnificPopup({
+        type: "image",
+        gallery: {
+          enabled: true,
+        },
+      });
+  });
+
   var map;
 
   function initialize() {
     var mapOptions = {
       zoom: 13,
-      center: new google.maps.LatLng(10.136344, -85.799155),
+      center: new google.maps.LatLng(50.97797382271958, -114.107718560791),
       // styles: style_array_here
     };
     map = new google.maps.Map(
@@ -105,4 +174,27 @@ AOS.init({
   if (google_map_canvas.length) {
     google.maps.event.addDomListener(window, "load", initialize);
   }
-})();
+
+  // Counter
+
+  $(".counter-stat").counterUp({
+    delay: 10,
+    time: 1000,
+  });
+
+  // Shuffle js filter and masonry
+  var Shuffle = window.Shuffle;
+  var jQuery = window.jQuery;
+
+  var myShuffle = new Shuffle(document.querySelector(".shuffle-wrapper"), {
+    itemSelector: ".shuffle-item",
+    buffer: 1,
+  });
+
+  jQuery('input[name="shuffle-filter"]').on("change", function (evt) {
+    var input = evt.currentTarget;
+    if (input.checked) {
+      myShuffle.filter(input.value);
+    }
+  });
+})(jQuery);
